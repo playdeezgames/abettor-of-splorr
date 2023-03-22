f81b9b885800d9084e5d25a8ae5509ec43701bb8
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-03-22 05:29:50 -0500

    host dont care how the buffer gets updated.

--- src/AOS/Host.vb
+++ src/AOS/Host.vb
@@ -1,22 +1,49 @@
 ﻿Public Class Host
     Inherits Game
-    Private graphics As GraphicsDeviceManager
-    Private spriteBatch As SpriteBatch
-    Sub New()
-        graphics = New GraphicsDeviceManager(Me)
+    Private ReadOnly _windowWidth As Integer
+    Private ReadOnly _windowHeight As Integer
+    Private ReadOnly _viewWidth As Integer
+    Private ReadOnly _viewHeight As Integer
+    Private ReadOnly _graphics As GraphicsDeviceManager
+    Private _texture As Texture2D
+    Private _spriteBatch As SpriteBatch
+    Private _buffer As Color()
+    Private _updatifier As Action(Of Color(), Integer, Integer)
+    Sub New(
+           windowWidth As Integer,
+           windowHeight As Integer,
+           viewWidth As Integer,
+           viewHeight As Integer,
+           updatifier As Action(Of Color(), Integer, Integer))
+        _graphics = New GraphicsDeviceManager(Me)
+        _windowHeight = windowHeight
+        _windowWidth = windowWidth
+        _viewWidth = viewWidth
+        _viewHeight = viewHeight
+        _updatifier = updatifier
+        ReDim _buffer(_viewWidth * _viewHeight - 1)
         Content.RootDirectory = "Content"
     End Sub
     Protected Overrides Sub Initialize()
+        _graphics.PreferredBackBufferWidth = _windowWidth
+        _graphics.PreferredBackBufferHeight = _windowHeight
+        _graphics.ApplyChanges()
         MyBase.Initialize()
     End Sub
     Protected Overrides Sub LoadContent()
-        spriteBatch = New SpriteBatch(GraphicsDevice)
+        _spriteBatch = New SpriteBatch(GraphicsDevice)
+        _texture = New Texture2D(GraphicsDevice, _viewWidth, _viewHeight)
     End Sub
     Protected Overrides Sub Update(gameTime As GameTime)
+        _updatifier(_buffer, _viewWidth, _viewHeight)
+        _texture.SetData(_buffer)
         MyBase.Update(gameTime)
     End Sub
     Protected Overrides Sub Draw(gameTime As GameTime)
-        graphics.GraphicsDevice.Clear(Color.Magenta)
+        _graphics.GraphicsDevice.Clear(Color.Magenta)
+        _spriteBatch.Begin(samplerState:=SamplerState.PointClamp)
+        _spriteBatch.Draw(_texture, New Rectangle(0, 0, _windowWidth, _windowHeight), Nothing, Color.White)
+        _spriteBatch.End()
         MyBase.Draw(gameTime)
     End Sub
 End Class
--- src/AOS/Program.vb
+++ src/AOS/Program.vb
@@ -1,8 +1,9 @@
-Imports System
-
 Module Program
+    Private Sub Updatifier(buffer As Color(), width As Integer, height As Integer)
+        buffer(0) = New Color(0, 0, 0, 255)
+    End Sub
     Sub Main(args As String())
-        Using host As New Host()
+        Using host As New Host(1280, 720, 160, 90, AddressOf Updatifier)
             host.Run()
         End Using
     End Sub
