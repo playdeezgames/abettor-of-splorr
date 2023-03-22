3e7480617451451dd618161de7d2f9247109aee2
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-03-22 05:53:43 -0500

    very simple monogame host

--- src/AOS/Host.vb
+++ src/AOS/Host.vb
@@ -7,20 +7,24 @@
     Private ReadOnly _graphics As GraphicsDeviceManager
     Private _texture As Texture2D
     Private _spriteBatch As SpriteBatch
-    Private _buffer As Color()
-    Private _updatifier As Action(Of Color(), Integer, Integer)
+    Private ReadOnly _buffer As Color()
+    Private ReadOnly _updatifier As Action(Of Color(), Integer, Integer)
+    Private ReadOnly _commanderator As Action(Of Keys())
+    Private _keyboardState As KeyboardState
     Sub New(
            windowWidth As Integer,
            windowHeight As Integer,
            viewWidth As Integer,
            viewHeight As Integer,
-           updatifier As Action(Of Color(), Integer, Integer))
+           updatifier As Action(Of Color(), Integer, Integer),
+           commanderator As Action(Of Keys()))
         _graphics = New GraphicsDeviceManager(Me)
         _windowHeight = windowHeight
         _windowWidth = windowWidth
         _viewWidth = viewWidth
         _viewHeight = viewHeight
         _updatifier = updatifier
+        _commanderator = commanderator
         ReDim _buffer(_viewWidth * _viewHeight - 1)
         Content.RootDirectory = "Content"
     End Sub
@@ -28,6 +32,7 @@
         _graphics.PreferredBackBufferWidth = _windowWidth
         _graphics.PreferredBackBufferHeight = _windowHeight
         _graphics.ApplyChanges()
+        _keyboardState = Keyboard.GetState
         MyBase.Initialize()
     End Sub
     Protected Overrides Sub LoadContent()
@@ -35,6 +40,9 @@
         _texture = New Texture2D(GraphicsDevice, _viewWidth, _viewHeight)
     End Sub
     Protected Overrides Sub Update(gameTime As GameTime)
+        Dim newState = Keyboard.GetState()
+        _commanderator(newState.GetPressedKeys().Where(Function(k) _keyboardState.IsKeyUp(k)).ToArray)
+        _keyboardState = newState
         _updatifier(_buffer, _viewWidth, _viewHeight)
         _texture.SetData(_buffer)
         MyBase.Update(gameTime)
--- src/AOS/Program.vb
+++ src/AOS/Program.vb
@@ -1,10 +1,27 @@
 Module Program
+    Private x As Integer = 0
+    Private y As Integer = 0
     Private Sub Updatifier(buffer As Color(), width As Integer, height As Integer)
-        buffer(0) = New Color(0, 0, 0, 255)
+        buffer(x + y * width) = New Color(0, 0, 0, 255)
     End Sub
     Sub Main(args As String())
-        Using host As New Host(1280, 720, 160, 90, AddressOf Updatifier)
+        Using host As New Host(1280, 720, 160, 90, AddressOf Updatifier, AddressOf Commanderator)
             host.Run()
         End Using
+    End Sub
+
+    Private Sub Commanderator(pressedKeys() As Keys)
+        For Each pressedKey In pressedKeys
+            Select Case pressedKey
+                Case Keys.Up
+                    y -= 1
+                Case Keys.Down
+                    y += 1
+                Case Keys.Left
+                    x -= 1
+                Case Keys.Right
+                    x += 1
+            End Select
+        Next
     End Sub
 End Module
