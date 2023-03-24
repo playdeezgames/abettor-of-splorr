185ddfaf95f227b4015545ac620ff05219cef46d
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-03-24 05:26:49 -0500

    move base buffer into ui assembly

---
+++ src/AOS.UI/HueBuffer.vb
@@ -0,0 +1,10 @@
+﻿Public Class HueBuffer(Of THue)
+    Inherits BaseDisplayBuffer(Of THue, THue)
+
+    Public Sub New(size As (Integer, Integer))
+        MyBase.New(size, Function(x) x)
+    End Sub
+
+    Public Overrides Sub Commit()
+    End Sub
+End Class
