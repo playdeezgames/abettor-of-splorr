3e1ae9c3afbe7afd706fbe78ee2b20923b5bedea
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-03-25 04:34:21 -0500

    i invented thescreen save!

--- src/AOS.Application/GameController.vb
+++ src/AOS.Application/GameController.vb
@@ -1,29 +1,38 @@
 ﻿Public Class GameController
     Inherits BaseGameController
-    Private _configSink As Action(Of (Integer, Integer), Single)
+    Private ReadOnly _configSink As Action(Of (Integer, Integer), Single)
+    Private ReadOnly sprite As OffscreenBuffer(Of Hue)
     Private x As Integer = 0
     Private y As Integer = 0
+    Private deltaX As Integer = 1
+    Private deltaY As Integer = 1
+    Const spriteWidth = 8
+    Const spriteHeight = 8
+    Const viewWidth = 160
+    Const viewHeight = 90
 
     Public Sub New(windowSizeSource As Func(Of (Integer, Integer)), volumeSource As Func(Of Single), configSink As Action(Of (Integer, Integer), Single))
         MyBase.New(windowSizeSource(), volumeSource())
         _configSink = configSink
         _configSink(Size, Volume)
+        sprite = New OffscreenBuffer(Of Hue)((spriteWidth, spriteHeight))
+        For spriteX = 0 To spriteWidth - 1
+            For spriteY = 0 To spriteHeight - 1
+                sprite.SetPixel(spriteX, spriteY, CType((spriteX + spriteY) Mod 15, Hue))
+            Next
+        Next
     End Sub
 
     Public Overrides Sub HandleCommand(command As Command)
-        Select Case command
-            Case Command.Up
-                y -= 1
-            Case Command.Down
-                y += 1
-            Case Command.Left
-                x -= 1
-            Case Command.Right
-                x += 1
-        End Select
     End Sub
 
     Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
-        displayBuffer.SetPixel(x, y, Hue.Blue)
+        displayBuffer.Copy(sprite, (0, 0), (x, y), (spriteWidth, spriteHeight), Function(x) True)
+        x = x + deltaX
+        y = y + deltaY
+        If x = 0 Then deltaX = Math.Abs(deltaX)
+        If y = 0 Then deltaY = Math.Abs(deltaY)
+        If x = viewWidth - spriteWidth Then deltaX = -Math.Abs(deltaX)
+        If y = viewHeight - spriteHeight Then deltaY = -Math.Abs(deltaY)
     End Sub
 End Class
