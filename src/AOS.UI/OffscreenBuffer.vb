0ab5ff0511cbddc3d8fe2bddb0606bb87666c054
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-03-26 04:21:58 -0500

    loading an offscreen buffer from a string array

--- src/AOS.UI/OffscreenBuffer.vb
+++ src/AOS.UI/OffscreenBuffer.vb
@@ -10,8 +10,19 @@
     Public Overrides Sub SetPixel(x As Integer, y As Integer, hue As THue)
         _buffer(x + y * _size.Item1) = hue
     End Sub
-
     Public Function GetPixel(x As Integer, y As Integer) As THue Implements IPixelSource(Of THue).GetPixel
         Return _buffer(x + y * _size.Item1)
+    End Function
+    Public Shared Function Create(transform As Func(Of Char, THue), ParamArray lines As String()) As OffscreenBuffer(Of THue)
+        Dim buffer As New OffscreenBuffer(Of THue)((lines.First.Length, lines.Length))
+        For y = 0 To lines.Length - 1
+            Dim line = lines(y)
+            Dim x = 0
+            For Each character In line
+                buffer.SetPixel(x, y, transform(character))
+                x += 1
+            Next
+        Next
+        Return buffer
     End Function
 End Class
