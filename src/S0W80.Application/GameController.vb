445d69a0f0626095c5a456157b4dc86f4e1fc3e3
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-12 03:52:30 -0500

    it repeats what i type

--- src/S0W80.Application/GameController.vb
+++ src/S0W80.Application/GameController.vb
@@ -1,6 +1,7 @@
 Public Class GameController
     Implements IGameController
     Private ReadOnly _frameBuffer As IFrameBuffer
+    Private _lineBuffer As String = String.Empty
     Sub New(frameBuffer As IFrameBuffer)
         _frameBuffer = frameBuffer
     End Sub
@@ -12,12 +13,23 @@ Public Class GameController
                         _frameBuffer.CursorColumn -= 1
                         _frameBuffer.Write(" "c)
                         _frameBuffer.CursorColumn -= 1
+                        _lineBuffer = If(_lineBuffer.Any, _lineBuffer.Substring(0, _lineBuffer.Length - 1), _lineBuffer)
                     Case ChrW(13)
                         _frameBuffer.WriteLine("")
+                        DoStuff(_lineBuffer)
+                        _lineBuffer = String.Empty
                     Case Else
                         _frameBuffer.Write(command)
+                        _lineBuffer &= command
                 End Select
             End If
         Next
+    End Sub
+
+    Private Sub DoStuff(lineBuffer As String)
+        Dim oldColor = _frameBuffer.ForegroundColor
+        _frameBuffer.ForegroundColor = 15
+        _frameBuffer.WriteLine(lineBuffer)
+        _frameBuffer.ForegroundColor = oldColor
     End Sub
 End Class
