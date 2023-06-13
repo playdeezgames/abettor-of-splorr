54fed57a6c1cb00dd64229ca6179da97b683b5c8
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-13 04:57:17 -0500

    moved some constants

--- src/S0W80.Presentation/Constants.vb
+++ src/S0W80.Presentation/Constants.vb
@@ -1,9 +1,3 @@
 ﻿Public Module Constants
-    Friend Const CellWidth = 8
-    Friend Const CellHeight = 16
-    Public Const ScreenColumns = 90
-    Public Const ScreenRows = 25
-    Friend Const ScreenWidth = ScreenColumns * CellWidth
-    Friend Const ScreenHeight = ScreenRows * CellHeight
     Friend Const FontFilename = "Font8x16.png"
 End Module
---
+++ src/S0W80.UI/Constants.vb
@@ -0,0 +1,8 @@
+﻿Public Module Constants
+    Friend Const CellWidth = 8
+    Friend Const CellHeight = 16
+    Public Const ScreenColumns = 80
+    Public Const ScreenRows = 25
+    Public Const ScreenWidth = ScreenColumns * CellWidth
+    Public Const ScreenHeight = ScreenRows * CellHeight
+End Module
