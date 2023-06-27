d192a7dfbf8ba5ab33159c5ee1c1631d0babd77f
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-27 05:15:51 -0500

    hue table has replace hue transform

--- src/AOS.Presentation/DisplayBuffer.vb
+++ src/AOS.Presentation/DisplayBuffer.vb
@@ -2,12 +2,10 @@
     Inherits BasePixelSink
     Implements IDisplayBuffer
     Private ReadOnly _texture As Texture2D
-    Private ReadOnly _transform As Func(Of Integer, Color)
     Private ReadOnly _hueTable As IReadOnlyDictionary(Of Integer, Color)
     Protected _buffer As Color()
     Sub New(texture As Texture2D, transform As Func(Of Integer, Color), hueTable As IReadOnlyDictionary(Of Integer, Color))
         _texture = texture
-        _transform = transform
         _hueTable = hueTable
         ReDim _buffer(_texture.Width * _texture.Height - 1)
     End Sub
@@ -19,6 +17,6 @@
         If x < Zero OrElse y < Zero OrElse x >= _texture.Width OrElse y >= _texture.Height Then
             Return
         End If
-        _buffer(x + y * _texture.Width) = _transform(hue)
+        _buffer(x + y * _texture.Width) = _hueTable(hue)
     End Sub
 End Class
