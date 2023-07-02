d5e30dcf7133d3bf3d8a2d80620bcfd6eb107202
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-02 18:40:04 -0500

    save and volume

--- src/AOS.UI/Boilerplate/Menu/SaveState.vb
+++ src/AOS.UI/Boilerplate/Menu/SaveState.vb
@@ -1,17 +1,17 @@
 ﻿Friend Class SaveState
-    Inherits BasePickerState(Of String)
+    Inherits BasePickerState(Of Integer)
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), context As IUIContext)
         MyBase.New(parent, setState, context, "Save Game", context.ControlsText("Select", "Cancel"), BoilerplateState.GameMenu)
     End Sub
-    Protected Overrides Sub OnActivateMenuItem(value As (String, String))
+    Protected Overrides Sub OnActivateMenuItem(value As (String, Integer))
         Dim slotIndex = CInt(value.Item2)
         Context.SaveGame(slotIndex)
         SetState(BoilerplateState.GameMenu)
     End Sub
-    Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
-        Dim result As New List(Of (String, String))
+    Protected Overrides Function InitializeMenuItems() As List(Of (String, Integer))
+        Dim result As New List(Of (String, Integer))
         For slotIndex = 1 To 5
-            result.Add(($"Slot {slotIndex}{If(Context.DoesSlotExist(slotIndex), "(will overwrite)", "")}", $"{slotIndex}"))
+            result.Add(($"Slot {slotIndex}{If(Context.DoesSlotExist(slotIndex), "(will overwrite)", "")}", slotIndex))
         Next
         Return result
     End Function
--- src/AOS.UI/Boilerplate/Menu/VolumeState.vb
+++ src/AOS.UI/Boilerplate/Menu/VolumeState.vb
@@ -1,22 +1,22 @@
 ﻿Friend Class VolumeState
-    Inherits BasePickerState(Of String)
+    Inherits BasePickerState(Of Single)
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), context As IUIContext)
         MyBase.New(parent, setState, context, "<placeholder>", context.ControlsText("Select", "Cancel"), BoilerplateState.Options)
     End Sub
-    Protected Overrides Sub OnActivateMenuItem(value As (String, String))
+    Protected Overrides Sub OnActivateMenuItem(value As (String, Single))
         Dim percent = CInt(value.Item2)
         Volume = percent * 0.01F
         PlaySfx(BoilerplateSfx.SfxVolumeTest)
         SaveConfig()
         HeaderText = $"Volume (Currently: {Volume * 100:f0}%)"
     End Sub
-    Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
+    Protected Overrides Function InitializeMenuItems() As List(Of (String, Single))
         Dim currentVolume = $"{Volume * 100:f0}%"
         HeaderText = $"Volume (Currently: {currentVolume})"
-        Dim result As New List(Of (String, String))
+        Dim result As New List(Of (String, Single))
         For index = 0 To 10
             Dim menuItemText = $"{index * 10}%"
-            result.Add((menuItemText, $"{index * 10}"))
+            result.Add((menuItemText, index / 10.0F))
             If menuItemText = currentVolume Then
                 MenuItemIndex = index
             End If
