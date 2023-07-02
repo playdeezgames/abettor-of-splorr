769a58b41c5a0fa38ade56a62a13ad4c46da7773
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-02 18:42:00 -0500

    window size

--- src/AOS.UI/Boilerplate/Menu/WindowSizeState.vb
+++ src/AOS.UI/Boilerplate/Menu/WindowSizeState.vb
@@ -1,20 +1,19 @@
 ﻿Friend Class WindowSizeState
-    Inherits BasePickerState(Of String)
+    Inherits BasePickerState(Of (Integer, Integer))
     Public Sub New(parent As IGameController, setState As Action(Of String, Boolean), context As IUIContext)
         MyBase.New(parent, setState, context, "<placeholder>", context.ControlsText("Select", "Cancel"), BoilerplateState.Options)
     End Sub
-    Protected Overrides Sub OnActivateMenuItem(value As (String, String))
-        Dim tokens = value.Item2.Split("x"c)
-        Dim width = CInt(tokens(0))
-        Dim height = CInt(tokens(1))
+    Protected Overrides Sub OnActivateMenuItem(value As (String, (Integer, Integer)))
+        Dim width = value.Item2.Item1
+        Dim height = value.Item2.Item2
         Parent.Size = (width, height)
         SaveConfig()
         HeaderText = $"Current Size: {Size.Item1}x{Size.Item2}"
     End Sub
-    Protected Overrides Function InitializeMenuItems() As List(Of (String, String))
+    Protected Overrides Function InitializeMenuItems() As List(Of (String, (Integer, Integer)))
         Dim currentSize = $"{Size.Item1}x{Size.Item2}"
         HeaderText = $"Current Size: {currentSize}"
-        Dim menuItems = Context.AvailableWindowSizes.Select(Function(x) ($"{x.Item1}x{x.Item2}", $"{x.Item1}x{x.Item2}")).ToList
+        Dim menuItems = Context.AvailableWindowSizes.Select(Function(x) ($"{x.Item1}x{x.Item2}", (x.Item1, x.Item2))).ToList
         MenuItemIndex = Math.Max(0, menuItems.FindIndex(Function(x) x.Item1 = currentSize))
         Return menuItems
     End Function
